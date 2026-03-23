cbf6e541181a0baf6ff0eff17a2b34d64ca77e32
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-03-22 21:01:35 -0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,5 @@
+[] Finish duck jump (see TODO file)
+[] Finish minnesota trail (see TODO file) 
+[] Python based flight sim route generator
+[] Python based flight sim perf calculator
+[] python based flight tracker
