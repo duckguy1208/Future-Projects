389ab881ca76771bf731e54db7fb4944d686b839
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-05-11 09:30:45 -0500

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,8 @@
+[x] Python GPA Calculator
 [] Finish duck jump (see TODO file)
 [] Finish minnesota trail (see TODO file) 
 [] Python based Fallout style game with pygame
 [] Python based flight sim route generator
 [] Python based flight sim perf calculator
 [] Python based flight tracker
-[x] Python GPA Calculator
+[] Carb Jet Size Calculator 
